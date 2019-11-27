6721ca0d8df3e58930fa12c89bb726fa7bbfb110
vida23 <vidageetirashidi@gmail.com>
2019-11-27 15:39:51 +0100

    Working on AddEventView - dismissing keyboard ( textView, textField ) - get date and time for the picker

--- BCalendar/AddEventView.swift
+++ BCalendar/AddEventView.swift
@@ -1,21 +1,49 @@
-//
-//  AddEventView.swift
-//  BCalendar
-//
-//  Created by Vida Rashidi on 2019-11-21.
-//  Copyright © 2019 Project. All rights reserved.
-//
 
 import UIKit
 
 class AddEventView: UIViewController {
 
+    @IBOutlet weak var DateTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var DateTimeField: UITextField!
+    @IBOutlet weak var eventDescription: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        DateTimeField.delegate = self
         
-
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy HH:mm"
+        DateTimeField.text = formatter.string(from: DateTimePicker.date)
+        //Format to strings
         
     }
     
-
+    
+    @IBAction func SelectedDateTime (_ : AnyObject){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy HH:mm"
+        DateTimeField.text = formatter.string(from: DateTimePicker.date)
+        
+    }
+    
+    
+//MARK: - Dismissing keyboards
+    //Dismisses the keyboard when editing has finished
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        //self.view.endEditing(true)
+        self.eventDescription.resignFirstResponder()
+    }
 }
+
+//Dismisses the keyboard when editing has finished
+extension AddEventView : UITextFieldDelegate {
+    
+    func textFieldShouldReturn (_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
